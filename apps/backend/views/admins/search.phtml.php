23399b5c7efbc72641de7af4ff20439d362fe0f8
root <root@fas-online.ru>
2015-12-28 11:53:07 +0000

    777

--- apps/backend/views/admins/search.phtml.php
+++ apps/backend/views/admins/search.phtml.php
@@ -1,5 +1,5 @@
-<?php $v4619004751iterated = false; ?><?php $v4619004751iterator = $page->items; $v4619004751incr = 0; $v4619004751loop = new stdClass(); $v4619004751loop->length = count($v4619004751iterator); $v4619004751loop->index = 1; $v4619004751loop->index0 = 1; $v4619004751loop->revindex = $v4619004751loop->length; $v4619004751loop->revindex0 = $v4619004751loop->length - 1; ?><?php foreach ($v4619004751iterator as $admin) { ?><?php $v4619004751loop->first = ($v4619004751incr == 0); $v4619004751loop->index = $v4619004751incr + 1; $v4619004751loop->index0 = $v4619004751incr; $v4619004751loop->revindex = $v4619004751loop->length - $v4619004751incr; $v4619004751loop->revindex0 = $v4619004751loop->length - ($v4619004751incr + 1); $v4619004751loop->last = ($v4619004751incr == ($v4619004751loop->length - 1)); ?><?php $v4619004751iterated = true; ?>
-<?php if ($v4619004751loop->first) { ?>
+<?php $v115050340928910814031iterated = false; ?><?php $v115050340928910814031iterator = $page->items; $v115050340928910814031incr = 0; $v115050340928910814031loop = new stdClass(); $v115050340928910814031loop->length = count($v115050340928910814031iterator); $v115050340928910814031loop->index = 1; $v115050340928910814031loop->index0 = 1; $v115050340928910814031loop->revindex = $v115050340928910814031loop->length; $v115050340928910814031loop->revindex0 = $v115050340928910814031loop->length - 1; ?><?php foreach ($v115050340928910814031iterator as $admin) { ?><?php $v115050340928910814031loop->first = ($v115050340928910814031incr == 0); $v115050340928910814031loop->index = $v115050340928910814031incr + 1; $v115050340928910814031loop->index0 = $v115050340928910814031incr; $v115050340928910814031loop->revindex = $v115050340928910814031loop->length - $v115050340928910814031incr; $v115050340928910814031loop->revindex0 = $v115050340928910814031loop->length - ($v115050340928910814031incr + 1); $v115050340928910814031loop->last = ($v115050340928910814031incr == ($v115050340928910814031loop->length - 1)); ?><?php $v115050340928910814031iterated = true; ?>
+<?php if ($v115050340928910814031loop->first) { ?>
 <table>
     <thead>
     <tr>
@@ -19,7 +19,7 @@
         <td><a href="/admin/admins/edit/<?php echo $admin->id; ?>">Редактировать</a></td>
         <td><a href="/admin/admins/del/<?php echo $admin->id; ?>">Удалить</a></td>
     </tr>
-    <?php if ($v4619004751loop->last) { ?>
+    <?php if ($v115050340928910814031loop->last) { ?>
     </tbody>
     <tbody>
     <tr>
@@ -36,6 +36,6 @@
     </tbody>
 </table>
 <?php } ?>
-<?php $v4619004751incr++; } if (!$v4619004751iterated) { ?>
+<?php $v115050340928910814031incr++; } if (!$v115050340928910814031iterated) { ?>
 В базе нет админов
 <?php } ?>
