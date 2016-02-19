560d5f107fbff619aa79e9e1d133a052750e8c96
Valentin <valentinkh1@gmail.com>
2016-02-19 16:37:55 +0200

    edit complaints answers

--- apps/frontend/models/Question.php
+++ apps/frontend/models/Question.php
@@ -2,6 +2,8 @@
 namespace Multiple\Frontend\Models;
 
 use Phalcon\Mvc\Model;
+use Multiple\Frontend\Models\Admin;
+use Multiple\Frontend\Models\Answer;
 
 class Question extends Model
 {
@@ -19,5 +21,30 @@ class Question extends Model
     public function getSource()
     {
         return "question";
+    }
+    public function getComplainQuestionAndAnswer($complaint_id){
+        $result = Question::find(
+            array(
+                "complaint_id = :complaint_id: ",
+                'bind' => array(
+                    'complaint_id' => $complaint_id
+                )
+            )
+        );
+        $qa = array();
+        $answerM = new Answer();
+        foreach($result as $v){
+
+            $answer = $answerM->findByQuestion($v->id);
+
+            $qa[] = array(
+                'question' => $v,
+                'answer' => $answer,
+                'admin' => (isset($answer['id']))?Admin::findFirstById($answer['admin_id']):false
+
+            );
+
+        }
+        return $qa;
     }
 }
