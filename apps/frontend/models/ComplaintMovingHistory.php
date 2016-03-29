e18edeee6e5dd1bce0210b56a1b88eb9c4432c0c
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-03-29 17:25:20 +0300

    #comliant moving history

---
+++ apps/frontend/models/ComplaintMovingHistory.php
@@ -0,0 +1,23 @@
+<?php
+namespace Multiple\Frontend\Models;
+
+use Phalcon\Mvc\Model;
+
+class ComplaintMovingHistory extends Model
+{
+    public $id;
+    public $complaint_id;
+    public $old_status;
+    public $new_status;
+    public $date;
+
+    public function initialize()
+    {
+        $this->setSource('complaint_moving_history');
+    }
+
+    public function getSource()
+    {
+        return 'complaint_moving_history';
+    }
+}
