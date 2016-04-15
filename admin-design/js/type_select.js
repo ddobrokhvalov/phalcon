35e6d2aadceb371ef054057e2f36307983cc99f8
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-15 15:15:07 +0300

    admin-design-files

---
+++ admin-design/js/type_select.js
@@ -0,0 +1,15 @@
+// 'select' options stylization and record the value in a hidden "select" 
+
+jQuery(document).ready(function($) {
+	$('.current-option').click(function(){
+        $(this).next('.custom-options').slideToggle();
+	    $('.current-option span').toggleClass('rotate-icon');
+	});
+	$('.custom-options li').click(function(){
+	    var choosenValue = $(this).attr('data-value');
+	    $(this).parent().parent().parent().find('.hidden-select').val(choosenValue).prop('selected', true);
+	    $(this).parent().parent().find('.current-option span').text($(this).text());
+	    $(this).parent().parent().find('.current-option').attr('data-value', choosenValue);
+	    $(this).parent().parent().find('.custom-options').hide();
+	});
+});
