f2dac4535966bb8d7b8c0964b0fd089990509e14
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-18 15:40:12 +0300

    admin-design-files

--- apps/backend/controllers/ControllerBase.php
+++ apps/backend/controllers/ControllerBase.php
@@ -1,14 +1,34 @@
 <?php
 namespace Multiple\Backend\Controllers;
 use Phalcon\Mvc\Controller;
+use Multiple\Backend\Models\Admin;
 
 class ControllerBase extends Controller
 {
 
     protected function initialize()
     {
-       // $this->tag->prependTitle('INVO | ');
-       // $this->view->setTemplateAfter('main');
+        $auth = $this->session->get('auth');
+        if (!$auth)
+            $user_id = false;
+        else
+            $user_id = $auth['id'];
+
+        if ($user_id) {
+            $this->user = Admin::findFirst(
+                array(
+                    "id = :id:",
+                    'bind' => array(
+                        'id' => $user_id,
+                    )
+                )
+            );
+            $this->view->user = $this->user;
+        }
+        else {
+            header('Location: http://'.$_SERVER['HTTP_HOST']);
+            exit;
+        }
     }
 
     protected function forward($uri)
--- apps/backend/views/layouts/admins.phtml
+++
@@ -1,3 +0,0 @@
-<h2>This is the "admins" controller layout!</h2>
-
-<?php echo $this->getContent(); ?>
