6eebf8b1268312fa04840de299c61b759ff042e0
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-19 18:15:06 +0300

    permisions

--- apps/backend/controllers/LawyerController.php
+++ apps/backend/controllers/LawyerController.php
@@ -2,7 +2,7 @@
 namespace Multiple\Backend\Controllers;
 
 use Phalcon\Mvc\Controller;
-class ControllerLawyer extends ControllerBase
+class LawyerController extends ControllerBase
 {
 
     public function indexAction(){
