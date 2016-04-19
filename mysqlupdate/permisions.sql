381b6a5e6bc95094d897e0c06c8e7cf73d778152
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-19 17:42:33 +0300

    permisions

---
+++ mysqlupdate/permisions.sql
@@ -0,0 +1,9 @@
+INSERT INTO `permission` (`id`, `name`, `name_ru`) VALUES (NULL, 'complaints', 'Жалобы');
+INSERT INTO `permission` (`id`, `name`, `name_ru`) VALUES (NULL, 'arguments', 'Доводы');
+INSERT INTO `permission` (`id`, `name`, `name_ru`) VALUES (NULL, 'applicants', 'Заявители');
+INSERT INTO `permission` (`id`, `name`, `name_ru`) VALUES (NULL, 'lawyer', 'Вопросы юристу');
+
+INSERT INTO `permission_admin` (`id`, `admin_id`, `permission_id`, `read`, `edit`) VALUES (NULL, '1', '9', '1', '1');
+INSERT INTO `permission_admin` (`id`, `admin_id`, `permission_id`, `read`, `edit`) VALUES (NULL, '1', '10', '1', '1');
+INSERT INTO `permission_admin` (`id`, `admin_id`, `permission_id`, `read`, `edit`) VALUES (NULL, '1', '11', '1', '1');
+INSERT INTO `permission_admin` (`id`, `admin_id`, `permission_id`, `read`, `edit`) VALUES (NULL, '1', '12', '1', '1');
