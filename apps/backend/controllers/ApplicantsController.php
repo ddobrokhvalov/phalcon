c6b3e4740d9965421a9005862c8773cef72801ae
vadim-job-hg <vadim-job-hg@yandex.ru>
2016-04-21 15:51:50 +0300

    Log page design

--- apps/backend/controllers/ApplicantsController.php
+++ apps/backend/controllers/ApplicantsController.php
@@ -2,9 +2,21 @@
 namespace Multiple\Backend\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+use Multiple\Backend\Models\Applicant;
+use Multiple\Library\PaginatorBuilder;
 class ApplicantsController  extends ControllerBase
 {
     public function indexAction(){
-
+        $numberPage = isset($_GET['page']) ? $_GET['page'] : 1;
+        $Applicant = Applicant::find();
+        $paginator = new Paginator(array(
+            "data"  => $Applicant,
+            "limit" => 10,
+            "page"  => $numberPage
+        ));
+        $pages = $paginator->getPaginate();
+        $this->view->page = $pages;
+        $this->view->paginator_builder = PaginatorBuilder::buildPaginationArray($numberPage, $pages->total_pages);
     }
 }
