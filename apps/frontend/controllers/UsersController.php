29b5371a706709e924fd579de7e56dff4914e3f4
olezhkafp <olezhkafp@gmail.com>
2016-06-17 15:50:05 +0300

    Frontend: Change user password popup. Moving flashsession message to header.

--- apps/frontend/controllers/UsersController.php
+++ apps/frontend/controllers/UsersController.php
@@ -3,6 +3,7 @@
 namespace Multiple\Frontend\Controllers;
 
 use Phalcon\Mvc\Controller;
+use Multiple\Frontend\Models\User;
 
 class UsersController extends Controller
 {
@@ -11,4 +12,41 @@ class UsersController extends Controller
 	{
 		echo '<br>', __METHOD__;
 	}
+
+    public function changePasswordAction() {
+        $redirect_to = $this->request->getPost('current_path');
+        if (!isset($redirect_to) || !$redirect_to) {
+            $redirect_to = '/complaint/index';
+        }
+        $old_password = $this->request->getPost('old_password');
+        $new_password = $this->request->getPost('new_password');
+        $new_password_confirm = $this->request->getPost('new_password_confirm');
+        if (!isset($old_password) || (isset($old_password)) && !strlen($old_password)) {
+            $this->flashSession->error('Старый пароль не введен');
+            return $this->response->redirect($redirect_to);
+        }
+        if (isset($new_password) && $new_password) {
+            if ($new_password != $new_password_confirm) {
+                $this->flashSession->error('Подтвердите ввод нового пароля');
+                return $this->response->redirect($redirect_to);
+            }
+        } else {
+            $this->flashSession->error('Новый пароль не может быть пустым');
+            return $this->response->redirect($redirect_to);
+        }
+        if (isset($this->session->get('auth')['id'])) {
+            $user = User::findFirstById($this->session->get('auth')['id']);
+            if ($user) {
+                if ($user->password == sha1($old_password)) {
+                    $user->password = sha1($new_password);
+                    $user->update();
+                    $this->flashSession->success('Пароль изменен');
+                    return $this->response->redirect($redirect_to);
+                } else {
+                    $this->flashSession->error('Старый пароль введен не верно');
+                    return $this->response->redirect($redirect_to);
+                }
+            }
+        }
+    }
 }
