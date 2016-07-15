b06775c52f5b423710ccc8ad4bdeb48fb2c9b659
VadimAnt <valentinkh1@gmail.com>
2016-07-15 17:52:43 +0300

    Merge branch 'develop'
    # Conflicts:
    #	apps/backend/controllers/ControllerBase.php

--- public/js/index_complaint.js
+++ public/js/index_complaint.js
@@ -16,11 +16,14 @@ $(document).ready(function () {
     });
 
     $('.change-status').click(function () {
+        debugger;
         var status = $(this).attr("value");
-
-        indexComplaint.changeStatus(status);
+        if(!$(this).hasClass("button_copy_deactive")){
+            indexComplaint.changeStatus(status);
+        }
     });
     $('.button-copy').click(function(){
+        debugger;
         if(indexComplaint.copyButton === true && indexComplaint.selectedComplaint.length == 1){
             indexComplaint.changeStatus('copy');
         }
@@ -47,6 +50,7 @@ $(document).ready(function () {
 
 var indexComplaint = {
     selectedComplaint: [],
+    arrComplaint: [],
     copyButton: false,
     returnButton:false,
     inArchivButton:false,
@@ -72,17 +76,34 @@ var indexComplaint = {
         });
     },
     activeButton: function (button) {
-        this.copyButton = true;
+        if(button == '.button-copy'){
+            this.copyButton = true;
+        }
+        if(button == '.to-archive'){
+            this.inArchivButton = true;
+        }
+        if(button == '.to-delete'){
+            this.deleteButton = true;
+        }
         $(button).removeClass('button_copy_deactive');
         $(button).addClass('button_copy_active');
     },
     deActiveButton: function (button) {
-        this.copyButton = false;
+        if(button == '.button-copy'){
+            this.copyButton = false;
+        }
+        if(button == '.to-archive'){
+            this.inArchivButton = false;
+        }
+        if(button == '.to-delete'){
+            this.deleteButton = false;
+        }
         $(button).removeClass('button_copy_active');
         $(button).addClass('button_copy_deactive');
     },
     changeStatus: function (status) {
         debugger;
+        console.log(this);
         var data = JSON.stringify(this.selectedComplaint);
         var edit = $(".c-jadd1-1-b").attr("edit");
         if(edit != undefined){
@@ -165,43 +186,143 @@ var indexComplaint = {
         });
     },
     addComplain: function (id) {
+        debugger;
         this.selectedComplaint.push(id);
-        if (this.selectedComplaint.length > 0) {
-            if (jQuery("input[name=jlist" + id + "]").parent().find('#current-status').val() != "archive") {
+        this.arrComplaint.push({
+            'id':id,
+            'status': jQuery("input[name=jlist" + id + "]").parent().find('#current-status').val()
+        });
+
+
+        if(this.arrComplaint.length == 0){
+            $(".c-cs-btns").removeClass("c-cs-btns-after");
+            $(".button_copy_active").removeClass("button_copy_active");
+        }
+
+        if(this.arrComplaint.length == 1){
+            if(this.arrComplaint[0].status == 'draft' || this.arrComplaint[0].status == 'archive'){
+                this.activeButton('.button-copy');
+            }
+            if(this.arrComplaint[0].status == 'draft'){
                 this.activeButton('.to-archive');
             }
-            this.activeButton('.set-active');
+            if(this.arrComplaint[0].status == 'archive'){
+                this.deActiveButton('.to-archive');
+            }
         }
-        if (this.selectedComplaint.length == 1) {
-            this.activeButton('.button-copy');
-            if(currentStatus == 'submitted')
-              this.activeButton('.button-recall');
 
-        }else {
+        if(this.arrComplaint.length > 1){
             this.deActiveButton('.button-copy');
-            this.deActiveButton('.to-archive');
-            //if(currentStatus == 'submitted')
-               // this.deActiveButton('.button-recall');
+            //$(".button-copy ").removeClass("button_copy_active");
+
+            var checkSame = true;
+            var current = this.arrComplaint[0].status;
+            var archive = false;
+            var draft   = false;
+            for(var i = 0; i < this.arrComplaint.length; i++){
+                if(this.arrComplaint[i].status == 'draft')     draft = true;
+                if(this.arrComplaint[i].status == 'archive')   archive = true;
+                if(this.arrComplaint[i].status != current){
+                    checkSame = false;
+                }
+            }
+
+            if(checkSame && draft){
+                this.activeButton('.to-archive');
+            }
+            if(draft && archive){
+                this.deActiveButton('.to-archive');
+            }
         }
+
+
+        //if (this.selectedComplaint.length > 0) {
+        //    if (jQuery("input[name=jlist" + id + "]").parent().find('#current-status').val() != "archive") {
+        //        this.activeButton('.to-archive');
+        //        $(".to-archive").addClass("button_copy_active");
+        //    }
+        //    this.activeButton('.set-active');
+        //}
+        //if (this.selectedComplaint.length == 1) {
+        //    this.activeButton('.button-copy');
+        //    if(currentStatus == 'submitted')
+        //      this.activeButton('.button-recall');
+        //
+        //}else {
+        //    this.deActiveButton('.button-copy');
+        //    this.deActiveButton('.to-archive');
+        //    //if(currentStatus == 'submitted')
+        //       // this.deActiveButton('.button-recall');
+        //}
     },
     removeComplain: function (id) {
+        debugger;
         var index = this.selectedComplaint.indexOf(id);
         if (index > -1) {
             this.selectedComplaint.splice(index, 1);
         }
-        if (this.selectedComplaint.length == 1)
-            this.activeButton('.button-copy');
-        else
-            this.deActiveButton('.button-copy');
 
-        if(this.selectedComplaint.length < 1) {
-            this.deActiveButton('.button-recall');
-            this.deActiveButton('.to-archive');
+        if(this.arrComplaint.length > 0){
+            for(var i = 0; i < this.arrComplaint.length; i++){
+                if(this.arrComplaint[i].id == id) this.arrComplaint.splice(i, 1);
+            }
         }
 
+
+        if(this.arrComplaint.length == 0){
+            $(".c-cs-btns").removeClass("c-cs-btns-after");
+            $(".button_copy_active").removeClass("button_copy_active");
+        }
+
+        if(this.arrComplaint.length == 1){
+            if(this.arrComplaint[0].status == 'draft' || this.arrComplaint[0].status == 'archive'){
+                this.activeButton('.button-copy');
+            }
+            if(this.arrComplaint[0].status == 'draft'){
+                this.activeButton('.to-archive');
+            }
+            if(this.arrComplaint[0].status == 'archive'){
+                this.deActiveButton('.to-archive');
+            }
+        }
+
+        if(this.arrComplaint.length > 1){
+            this.deActiveButton('.button-copy');
+            //$(".button-copy ").removeClass("button_copy_active");
+
+            var checkSame = true;
+            var current = this.arrComplaint[0].status;
+            var archive = false;
+            var draft   = false;
+            for(var i = 0; i < this.arrComplaint.length; i++){
+                if(this.arrComplaint[i].status == 'draft')     draft = true;
+                if(this.arrComplaint[i].status == 'archive')   archive = true;
+                if(this.arrComplaint[i].status != current){
+                    checkSame = false;
+                }
+            }
+            if(checkSame && draft){
+                this.activeButton('.to-archive');
+            }
+            if(draft && archive){
+                this.deActiveButton('.to-archive');
+            }
+        }
+        //if (this.selectedComplaint.length == 1)
+        //    this.activeButton('.button-copy');
+        //else
+        //    this.deActiveButton('.button-copy');
+        //
+        //if(this.selectedComplaint.length < 1) {
+        //    this.deActiveButton('.button-recall');
+        //    this.deActiveButton('.to-archive');
+        //    $(".to-archive").removeClass("button_copy_active");
+        //}
     },
     selectAll: function () {
+        debugger;
         this.deActiveButton('.button-copy');
+        $(".to-archive").addClass("button_copy_active");
         if(currentStatus == 'submitted')
             this.activeButton('.button-recall');
         $('.complaint-checkbox').each(function () {
@@ -210,7 +331,9 @@ var indexComplaint = {
         });
     },
     deSelectAll: function () {
+        debugger;
         console.log(this.selectedComplaint);
+        $(".to-archive").removeClass("button_copy_active");
         if(currentStatus == 'submitted')
           this.deActiveButton('.button-recall');
         this.selectedComplaint = [];
