9cc9d45b8df666ce65ac56a42fe6ebfbce19fc81
VadimAnt <valentinkh1@gmail.com>
2016-07-18 11:19:24 +0300

    Merge branch 'develop'
    # Conflicts:
    #	apps/backend/controllers/ControllerBase.php

--- apps/backend/controllers/AdminsController.php
+++ apps/backend/controllers/AdminsController.php
@@ -140,6 +140,19 @@ class AdminsController extends ControllerBase
 
     public function addAction()
     {
+        $params = $this->dispatcher->getParams();
+        if(!empty($params)) $this->view->params = $params;
+        else {
+            $this->view->params = array(
+                'email'         => '',
+                'password'      => '',
+                'name'          => '',
+                'surname'       => '',
+                'phone'         => '',
+                'patronymic'    => '',
+                'avatar'        => '',
+            );
+        }
         if ($this->user->id != 1) {
             $this->view->pick("access/denied");
             $this->setMenu();
@@ -152,8 +165,33 @@ class AdminsController extends ControllerBase
             if (!$this->request->isPost())
                 return $this->forward("admins/index");
 
-            $admin = new Admin();
             $post = $this->request->getPost();
+
+            $checkEmail = Admin::findFirst("email = '{$post['email']}'");
+            if(!$checkEmail == false) {
+                $this->flashSession->error('Пользователь с таким email уже есть!');
+                $admin = new Admin();
+                if ($this->request->hasFiles() == true)
+                    if($admin->saveAvatar($this->request->getUploadedFiles()))
+                        $avatar = $admin->avatar;
+
+                return $this->dispatcher->forward(array(
+                    'module' => 'backend',
+                    'controller' => 'admins',
+                    'action' => 'add',
+                    'params' => array(
+                        'email'         => $post['email'],
+                        'password'      => $post['password'],
+                        'name'          => $post['name'],
+                        'surname'       => $post['surname'],
+                        'phone'         => $post['phone'],
+                        'patronymic'    => $post['patronymic'],
+                        'avatar'        => (!isset($avatar)) ? '' : $avatar
+                    )
+                ));
+            }
+
+            $admin = new Admin();
             $data['email'] = $post['email'];
             $data['admin_status'] = 0;
             $data['password'] = $post['password'];
