b5d33f3bc4688d6a25e70645709b5827a12cfb71
merush <andrew.fisto@gmail.com>
2016-07-29 16:40:15 +0300

    Feature#2/alfa/2.6

--- public/js/category_add.js
+++ public/js/category_add.js
@@ -8,7 +8,16 @@ $(document).ready(function() {
     $('.saveCat').click(function() {
         categorySend();
     });
+    $('.argCatTree').on('click', '.category_delete', function() {
+        var thisParent = $(this).parent().parent;
+        deleteCatBlock();
+    });
 });
+
+function popupCancel() {
+    $('.admin-popup-wrap').fadeOut();
+    $('.inputBox input').val('');
+}
 
 function categorySend() {
     var catName = $('.inputBox input').val(),
@@ -16,27 +25,27 @@ function categorySend() {
     createNewCategory.newCategorySend(data);
 }
 
-function popupCancel() {
-    $('.admin-popup-wrap').fadeOut();
-    $('.inputBox input').val('');
+function deleteCatBlock() {
+
 }
 
-var shell = new ShellToFill();
+var shell;
 function ShellToFill(step, titleText, id, parent_id) {
     this.wrapp = '<li class="catArguments">';
     this.holder = '<ul class="subWrap_' + step + '">';
     this.box = '<div class="category" data-id="' + id + '" data-parent_id="' + parent_id + '">';
+    this.box2 = '<li class="category" data-id="' + id + '" data-parent_id="' + parent_id + '">';
     this.arrow = '<div class="category_arrow"></div>';
-    this.title1 = '<h2>' + titleText + '</h2>';
+    this.title = '<h2>' + titleText + '</h2>';
     this.title2 = '<h3>' + titleText + '</h3>';
     this.catAdd = '<div class="category_add">Добавить категорию</div>';
-    this.catAddnt = '<div class="category_add withoutText"></div>';
+    this.catAdd2 = '<div class="category_add withoutText"></div>';
     this.catAddCross = '<div class="category_add crossView">Подкатегорию</div>';
     this.argAdd = '<div class="category_argumentAdd"></div>';
     this.argAddCross = '<div class="category_argumentAdd crossView">Довод</div>';
     this.catEdit = '<div class="category_edit"></div>';
     this.catDel = '<div class="category_delete"></div>';
-};
+}
 
 var createNewCategory = {
     newCategorySend: function(data) {
@@ -46,9 +55,8 @@ var createNewCategory = {
             data: data,
             dataType: 'json',
             success: function(value) {
-                console.log(value);
-                ShellToFill(1, value.name, value.id, value.parent_id);
-                createNewCategory.createParentCategory();
+                shell = new ShellToFill('', value.name, value.id, value.parent_id);
+                createNewCategory.createCategory();
             },
             error: function(xhr) {
                 alert(xhr + 'Request Status: ' + xhr.status + ' Status Text: '
@@ -56,22 +64,40 @@ var createNewCategory = {
             }
         });
     },
-    createParentCategory: function() {
+    createCategory: function() {
         $('.admin-popup-wrap').fadeOut();
         $('.argCatTree').append(
             shell.wrapp +
-            '<ul>' +
             shell.box +
             shell.arrow +
-            shell.title1 +
+            shell.title +
             shell.catAdd +
             shell.catEdit +
             shell.catDel +
-            '</ul>' +
-            '</li>'
+            '</div></li>'
         );
+        $('.inputBox input').val('');
     }
 };
+
+var deleteCategory = {
+    deleteCategorySend: function(data) {
+        $.ajax({
+            type: "GET",
+            url: "http://fas/admin/arguments/ajaxAddCategory",
+            data: data,
+            dataType: 'json',
+            success: function(value) {
+                shell = new ShellToFill('', value.name, value.id, value.parent_id);
+                createNewCategory.createCategory();
+            },
+            error: function(xhr) {
+                alert(xhr + 'Request Status: ' + xhr.status + ' Status Text: '
+                    + xhr.statusText + ' ResponseText:' + xhr.responseText);
+            }
+        });
+    }
+}
 
 
 /* /ajaxRemove?edit[id]=6&edit[name]=jhbnji&edit[arg]=true&edit[text]=mkonkomko */
