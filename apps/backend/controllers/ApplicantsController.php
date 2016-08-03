b457fd178487e02075f91c82772bfe031252aae8
VadimAnt <valentinkh1@gmail.com>
2016-08-03 11:17:50 +0300

    Merge branch 'develop'
    # Conflicts:
    #	public/css/style.css

--- apps/backend/controllers/ApplicantsController.php
+++ apps/backend/controllers/ApplicantsController.php
@@ -365,13 +365,14 @@ class ApplicantsController  extends ControllerBase
                 foreach ($this->request->getUploadedFiles() as $file) {
                     if (strlen($file->getName())) {
                         $applicant_file = new Files();
-                        $applicant_file->file_path = $file->getName();
+                        $applicant_file->file_path = $this->translit( $file->getName() );
                         $applicant_file->file_size = round($file->getSize() / 1024, 2);
                         $applicant_file->file_type = $file->getType();
                         $applicant_file->save();
                         $saved_files[] = $applicant_file->id;
                         //Move the file into the application
-                        $file->moveTo($baseLocation . $file->getName());
+                        $str = $baseLocation . $this->translit($file->getName());
+                        $file->moveTo($str);
                     }
                 }
             }
@@ -472,5 +473,11 @@ class ApplicantsController  extends ControllerBase
         $this->flashSession->success('Изменения сохранены');
         $data = "ok";
         echo json_encode($data);
+    }
+
+    private function translit( $str ) {
+        $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
+        $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
+        return str_replace($rus, $lat, $str);
     }
 }
