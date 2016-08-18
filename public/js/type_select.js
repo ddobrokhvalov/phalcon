5c8fe3f3c6dc6310b95833f2b4613553cb16217c
merush <andrew.fisto@gmail.com>
2016-08-18 17:48:36 +0300

    1

--- public/js/type_select.js
+++ public/js/type_select.js
@@ -1,18 +1,25 @@
-// 'select' options stylization and record the value in a hidden "select" 
-
 jQuery(document).ready(function($) {
-	$('.current-option').click(function(){
+	$('.current-option').click(function() {
         $(this).next('.custom-options').slideToggle();
 	    $(this).find('span').toggleClass('rotate-icon');
 	});
-
-	$('.custom-options').on('click', 'li', function(){
-	    var choosenValue = $(this).attr('data-value');
-	    $(this).parent().parent().parent().find('.hidden-select').val(choosenValue).prop('selected', true);
-	    $(this).parent().parent().find('.current-option span').text($(this).text());
-	    $(this).parent().parent().find('.current-option span').removeClass('rotate-icon');
-	    $(this).parent().parent().find('.current-option').attr('data-value', choosenValue);
-	    $(this).parent().parent().find('.custom-options').slideUp();
+	$('.custom-options').on('click', 'li', function() {
+		if ($(this).hasClass('selectArgType')) {
+			var choosenValueType = [];
+			choosenValueType.push($(this).attr('data-value'));
+			$(this).parent().parent().parent().find('.hidden-select').val(choosenValueType).prop('selected', true);
+			$(this).parent().parent().find('.current-option span').text($(this).text());
+			$(this).parent().parent().find('.current-option span').removeClass('rotate-icon');
+			$(this).parent().parent().find('.current-option').attr('data-value', choosenValueType);
+			$(this).parent().parent().find('.custom-options').slideUp();
+		} else {
+			var choosenValue = $(this).attr('data-value');
+			$(this).parent().parent().parent().find('.hidden-select').val(choosenValue).prop('selected', true);
+			$(this).parent().parent().find('.current-option span').text($(this).text());
+			$(this).parent().parent().find('.current-option span').removeClass('rotate-icon');
+			$(this).parent().parent().find('.current-option').attr('data-value', choosenValue);
+			$(this).parent().parent().find('.custom-options').slideUp();
+		}
 		if ($(this).hasClass('argo')) {
 			$(this).parent().parent().parent().parent().parent().parent().find('.hidden-select').val(choosenValue).prop('selected', true);
 			$(this).parent().parent().parent().parent().parent().find('.current-option span').text($(this).text());
@@ -22,5 +29,4 @@ jQuery(document).ready(function($) {
 			$(this).parent().parent().parent().parent().parent().find('.custom-options').slideUp();
 		}
 	});
-
 });
