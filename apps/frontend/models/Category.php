7fea3c4d3743dad303e081fa4ad300cd32b756c3
VadimAnt <valentinkh1@gmail.com>
2016-08-22 10:18:08 +0300

    Merge branch 'develop' of https://bitbucket.org/valentin_gubsky/fas into check-multi-select-type-AntropoV-16-08
    # Conflicts:
    #	apps/backend/controllers/ArgumentsController.php

--- apps/frontend/models/Category.php
+++ apps/frontend/models/Category.php
@@ -34,7 +34,7 @@ class Category extends Model
             $temp_arr = array();
             foreach($templates as $key => $val){
                 $temp =  preg_replace('/[\r\n\t]/', '', $val->text);
-                $val->text = 'fff';
+                $val->text = $temp;
                 $temp_arr[] = $val;
             }
            $category->cnt = $templates->count();
