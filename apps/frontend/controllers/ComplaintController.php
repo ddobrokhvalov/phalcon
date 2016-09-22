3dab597a27243725b4e57a53eef7f7cf1ec43305
VadimAnt <valentinkh1@gmail.com>
2016-09-22 17:54:03 +0300

    issue #126 AntropovV 12-09-16

--- apps/frontend/controllers/ComplaintController.php
+++ apps/frontend/controllers/ComplaintController.php
@@ -746,10 +746,6 @@ class ComplaintController extends ControllerBase
         exit;
     }
 
-    private function showRequiredOrNotRequired($arguments, $data){
-        if ($data['checkDate'] == 1 && $data['checkRequired'] == 0) $arguments->andWhere("required = 1");
-        if ($data['checkDate'] == 0) $arguments->andWhere("required = 0");
-    }
 
     private function checkTypePurchase($type ){
         $checkType = false;
@@ -799,5 +795,10 @@ class ComplaintController extends ControllerBase
         $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
         $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
         return str_replace($rus, $lat, $str);
+    }
+
+    private function showRequiredOrNotRequired($arguments, $data){
+        if ($data['checkDate'] == 1 && $data['checkRequired'] == 0) $arguments->andWhere("required = 1");
+        if ($data['checkDate'] == 0) $arguments->andWhere("required = 0");
     }
 }
