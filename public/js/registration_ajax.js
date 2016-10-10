6f5a417581028c3b27b0110eff636edc245396ee
VadimAnt <valentinkh1@gmail.com>
2016-10-10 17:10:48 +0300

    162 add config email 10/10/2016 Antropov

--- public/js/registration_ajax.js
+++ public/js/registration_ajax.js
@@ -8,24 +8,24 @@ $(document).ready(function () {
             dataType: "json",
             context: form,
             success: function(res){
-                if(res.status){
-                    switch(res.status){
-                        case 'ok':
-                            $(this).html('<strong style="font-size: 20px;">На указанный вами почтовый ящик отправлена ссылка для завершения регистрации</strong><br/>');
-                            $(this).append('<button>Закрыть</button>');
-                            break;
-                        case 'user exists':
-                            $(this).html('<strong style="font-size: 10px;">«Уже зарегистрированы – авторизуйтесь»</strong><br/>');
-                            $(this).append('<a >Закрыть</a>');
-                            break;
-
-                    }
-
-                }
-                if(res.status == 'ok'){
-                    $(this).html('<strong style="font-size: 10px;">На указанный вами почтовый ящик отправлена ссылка для завершения регистрации</strong><br/>');
-                    $(this).append('<button>Закрыть</button>');
-                }
+                // if(res.status){
+                //     switch(res.status){
+                //         case 'ok':
+                //             $(this).html('<strong style="font-size: 20px;">На указанный вами почтовый ящик отправлена ссылка для завершения регистрации</strong><br/>');
+                //             $(this).append('<button>Закрыть</button>');
+                //             break;
+                //         case 'user exists':
+                //             $(this).html('<strong style="font-size: 10px;">«Уже зарегистрированы – авторизуйтесь»</strong><br/>');
+                //             $(this).append('<a >Закрыть</a>');
+                //             break;
+                //
+                //     }
+                //
+                // }
+                // if(res.status == 'ok'){
+                //     $(this).html('<strong style="font-size: 10px;">На указанный вами почтовый ящик отправлена ссылка для завершения регистрации</strong><br/>');
+                //     $(this).append('<button>Закрыть</button>');
+                // }
             },
         });
         return false;
