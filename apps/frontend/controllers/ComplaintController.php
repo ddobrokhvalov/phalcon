b2d4c4d78fbf68e1272f97de50eb8b315d1fc6d5
VadimAnt <vadim-antropov@ukr.net>
2016-11-29 10:45:43 +0200

    fix dublicate

--- apps/frontend/controllers/ComplaintController.php
+++ apps/frontend/controllers/ComplaintController.php
@@ -1095,9 +1095,8 @@ class ComplaintController extends ControllerBase
 4. Локально - при помощи БЕСПЛАТНОЙ программы http://cryptoarm.ru/bitrix/redirect.php?event1=download&event2=cryptoarm5&goto=http://www.trusted.ru/wp-content/uploads/trusteddesktop.exe<br/>';
 
 
-        if($complaint->status != 'submitted') {
-            $this->SendToUfas($attached, $ufas->email, 'Жалоба 44-ФЗ', $content);
-        }
+        $this->SendToUfas($attached, $ufas->email, 'Жалоба 44-ФЗ', $content);
+
 
         echo json_encode(array(
             'status' => 'ok',
--- public/js/SendToUfas.js
+++ public/js/SendToUfas.js
@@ -1,19 +1,23 @@
+var countSend = 0;
 $(document).ready(function () {
     $('.podpisatEp').on('click', function () {
-        $.ajax({
-            type: 'POST',
-            url: '/complaint/sendComplaintToUfas',
-            data: {complId: $("#complaint_id").val()},
-            dataType: "json",
-            success: function (res) {
-                if(res.status == 'ok'){
-                    $('.send-uf').css({'display': 'none'});
-                    $('.send-suc').css({'display': 'flex'});
-                    setTimeout(function(){
-                        location.reload();
-                    }, 6000)
+        if(countSend == 0) {
+            countSend++;
+            $.ajax({
+                type: 'POST',
+                url: '/complaint/sendComplaintToUfas',
+                data: {complId: $("#complaint_id").val()},
+                dataType: "json",
+                success: function (res) {
+                    if (res.status == 'ok') {
+                        $('.send-uf').css({'display': 'none'});
+                        $('.send-suc').css({'display': 'flex'});
+                        setTimeout(function () {
+                            location.reload();
+                        }, 6000)
+                    }
                 }
-            }
-        });
+            });
+        }
     });
 });
