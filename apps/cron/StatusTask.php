0c2c9f180a9d3743500311fc3679d0c393a4e965
VadimAnt <vadim-antropov@ukr.net>
2016-12-01 16:16:48 +0200

    fix

--- apps/cron/StatusTask.php
+++ apps/cron/StatusTask.php
@@ -310,6 +310,7 @@ class StatusTask extends \Phalcon\Cli\Task{
 
         foreach ($complaints as $comp) {
             $applicant = Applicant::findFirst($comp->applicant_id);
+
             var_dump($comp->auction_id, $applicant->name_short, $comp->date_submit);
             $response = $this->getComplaint($comp->auction_id, $applicant->name_short, $comp->date_submit);
             if (!empty($response['complaint'])) {
