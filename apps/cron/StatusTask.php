ebc2935b9335b4f8f1d8657084f7c79679888193
VadimAnt <vadim-antropov@ukr.net>
2016-12-02 16:43:08 +0200

    fix parser

--- apps/cron/StatusTask.php
+++ apps/cron/StatusTask.php
@@ -310,7 +310,7 @@ class StatusTask extends \Phalcon\Cli\Task{
 
         foreach ($complaints as $comp) {
             $applicant = Applicant::findFirst($comp->applicant_id);
-            echo $comp->auction_id, $applicant->name_short, $comp->date_submit."\n";
+            echo $comp->auction_id." ".$applicant->name_short." ".$comp->date_submit."\n";
             $response = $this->getComplaint($comp->auction_id, $applicant->name_short, $comp->date_submit);
             //var_dump($response);
             if (!empty($response['complaint'])) {
