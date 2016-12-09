6d6f914e06f287e523587326322f2a1791305643
VadimAnt <vadim-antropov@ukr.net>
2016-12-09 14:21:45 +0200

    fix parser

--- apps/cron/StatusTask.php
+++ apps/cron/StatusTask.php
@@ -34,6 +34,7 @@ class StatusTask extends \Phalcon\Cli\Task{
             $applicant = Applicant::findFirst($comp->applicant_id);
             $parser = new Parser();
             $response = $parser->getComplaint((string)$comp->auction_id, (string)$applicant->name_short, (string)$comp->date_submit);
+            var_dump($response);
             if (!empty($response['complaint'])) {
                 $status = $response['complaint']['status'];
                 $result = (!empty($response['complaint']['rezultat_rassmotreniya'])) ? $response['complaint']['rezultat_rassmotreniya'] : '';
