0318fea38e3a41f9824e7c1e139f8155071f5d10
lamoid <wdb@mail.ru>
2017-04-25 21:05:56 +0500

    поле "Контактный факс, телефон" должно иметь формат ввода "+7 (ххх) ххх-хххх" и иметь валидацию на ввод цифр (можно только цифры)

--- public/js/applicant.js
+++ public/js/applicant.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $("#entity-phone").inputmask("+7 (999) 999 - 9999");
 
     $('.addAppCertificate__addBtn').click(function () {
         if (selectedCertif == false)
