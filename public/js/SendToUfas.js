321ac73e1400c3e6a113ba00a7a8711e06e0dc73
lamoid <wdb@mail.ru>
2017-05-18 20:13:45 +0500

    67i

--- public/js/SendToUfas.js
+++ public/js/SendToUfas.js
@@ -19,6 +19,26 @@ $(document).ready(function () {
                             location.reload();
                         }, 6000)
                     }
+                },
+                error: function (jqXHR, exception) {
+                    var msg = '';
+                    if (jqXHR.status === 0) {
+                        msg = 'Not connect.\n Verify Network.';
+                    } else if (jqXHR.status == 404) {
+                        msg = 'Requested page not found. [404]';
+                    } else if (jqXHR.status == 500) {
+                        msg = 'Internal Server Error [500].';
+                    } else if (exception === 'parsererror') {
+                        msg = 'Requested JSON parse failed.';
+                    } else if (exception === 'timeout') {
+                        msg = 'Time out error.';
+                    } else if (exception === 'abort') {
+                        msg = 'Ajax request aborted.';
+                    } else {
+                        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+                    }
+                   alert(msg);
+                   $('.loading_save').css({'display':'none'});
                 }
             });
         }
