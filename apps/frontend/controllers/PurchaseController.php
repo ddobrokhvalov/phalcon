bf1fe966092e4f6b1a51096d5ea0e13d0b5fe22a
Dmitry Dobrokhvalov <ddobrokhvalov@gmail.com>
2017-07-03 11:26:37 +0300

    add complaint from api

--- apps/frontend/controllers/PurchaseController.php
+++ apps/frontend/controllers/PurchaseController.php
@@ -67,8 +67,92 @@ class PurchaseController extends Controller
         $postData = $this->request->getPost();
         $auction_id = trim($postData['auction_id']);
         $purchase = new Parser();
-        $data = $purchase->parseAuction($auction_id);
-       // var_dump($data); exit;
+        //$data = $purchase->parseAuction($auction_id);
+		$xml_data = $purchase->getUrl("http://apis.multitender.ru/gospartner/getInfoByPurchaseNumber44?purchaseNumber=".$auction_id."&apikey=Xoh2liehai8Shoo4Gie1duoG8Iex7sea&format=xml");
+		if($xml_data){
+			$xml_data = simplexml_load_string($xml_data);
+			
+			$data = array(
+							'contact'=>array(
+											'dolg_lico'=>$xml_data->responsibleOrg->contactPerson->__toString(),
+											'email'=>$xml_data->responsibleOrg->contactEmail->__toString(),
+											'fax'=>$xml_data->responsibleOrg->contactPhone->__toString(),
+											'mesto_nahogdeniya'=>$xml_data->responsibleOrg->factAddress->__toString(),
+											'name'=>$xml_data->responsibleOrg->fullName->__toString(),
+											'pochtovy_adres'=>$xml_data->responsibleOrg->factAddress->__toString(),
+											'tel'=>$xml_data->responsibleOrg->contactPhone->__toString(),
+											),
+							'info'=>array(
+											'object_zakupki'=>$xml_data->purchaseObject->__toString(),
+											'platform'=>"",
+											'type'=>$xml_data->placingWay->__toString(),
+											'zakupku_osushestvlyaet'=>$xml_data->customer->fullname->__toString(),
+											'zakupku_osushestvlyaet_inn'=>$xml_data->customer->inn->__toString(),
+											),
+							'procedura'=>array(
+											'data_provedeniya'=>$xml_data->procedureInfo->bidding->__toString(),
+											'data_rassmotreniya'=>$xml_data->procedureInfo->scoring->__toString(),
+											'nachalo_podachi'=>$xml_data->procedureInfo->start->__toString(),
+											'okonchanie_podachi'=>$xml_data->procedureInfo->end->__toString(),
+											'okonchanie_rassmotreniya'=>$xml_data->procedureInfo->scoring->__toString(),
+											'poryadok_podachi'=>"",//"Участник закупки вправе подать заявку на участие в аукционе в любое время, с момента размещения извещения о его проведении в единой информационной системе до предусмотренных документацией о таком аукционе даты и времени окончания срока подачи заявок на участие в аукционе."
+											'vremya_provedeniya'=>"Время аукциона не определено",//"09:50"
+											'vskrytie_konvertov'=>$xml_data->procedureInfo->opening->__toString(),
+											),
+							'zakazchik'=>array(
+											'tel'=>$xml_data->customer->phone->__toString(),
+											'fax'=>$xml_data->customer->phone->__toString(),
+											'pochtovy_adres'=>$xml_data->customer->factualAddress->__toString(),
+											'email'=>$xml_data->customer->email->__toString(),
+											'kontaktnoe_lico'=>$xml_data->customer->contactPerson->__toString(),
+											),
+							);
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'открытый конкурс' || mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'закрытый аукцион'){
+				$data['procedura']['data_rassmotreniya'] = $xml_data->procedureInfo->scoring->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'электронный аукцион'){
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'конкурс с ограниченным участием'){
+				$data['procedura']['data_provedeniya'] = $xml_data->procedureInfo->prequalification->__toString();
+				$data['procedura']['data_rassmotreniya'] = $xml_data->procedureInfo->scoring->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'запрос котировок'){
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'повторный конкурс с ограниченным участием'){
+				$data['procedura']['data_provedeniya'] = $xml_data->procedureInfo->prequalification->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'закрытый конкурс'){
+				$data['procedura']['data_rassmotreniya'] = $xml_data->procedureInfo->scoring->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'закрытый конкурс с ограниченным участием'){
+				$data['procedura']['data_provedeniya'] = $xml_data->procedureInfo->prequalification->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'запрос предложений'){
+				$data['procedura']['data_rassmotreniya'] = $xml_data->procedureInfo->opening->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'предварительный отбор'){
+				$data['procedura']['data_provedeniya'] = $xml_data->procedureInfo->selecting->__toString();
+			}
+			
+			if(mb_strtolower( $data['info']['type'], mb_detect_encoding($data['info']['type']) ) == 'двухэтапный конкурс'){
+				$data['procedura']['data_rassmotreniya'] = $xml_data->procedureInfo->scoring->__toString();
+			}
+		}else{
+			$data = $purchase->parseAuction($auction_id);
+		}
+		/*print_r("<pre>");
+        print_r($data); 
+		print_r("<pre>");
+		exit;*/
         echo json_encode($data);
 
         exit;
